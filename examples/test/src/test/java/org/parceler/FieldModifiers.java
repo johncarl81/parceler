5ad0e8b18a98b569cb585bfa016b672ef540cbd8
John Ericksen <johncarl81@gmail.com>
2014-02-23 10:18:19 -0700

    Fixed public test

--- examples/test/src/test/java/org/parceler/FieldModifiers.java
+++ examples/test/src/test/java/org/parceler/FieldModifiers.java
@@ -20,7 +20,7 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 
 @Parcel
 public class FieldModifiers {
-    String one;
+    public String one;
     String two;
     protected String three;
     private String four;
